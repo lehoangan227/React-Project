b783df0ab21a1c2c10ac5806a80787c771fa1838
an_le 黎煌安 <an_le@accton.com.vn>
2025-11-21 15:08:22 +0700

    update 15

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,47 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './DisplayInfor.scss';
 import logo from './../logo.svg';
 
-// class DisplayInfor extends React.Component{
-    
-//     render(){
-//         console.log("Call me render")
-//         const {listUser} = this.props;
-//         // console.log(listUser);
-//         return (
-//             <div className="display-infor-container">
-//                 {true && 
-//                     <>
-//                         {
-//                             listUser.map((user)=>{
-//                                     return (
-//                                         <div key={user.id} className={parseInt(user.age) > 25 ? "red" : "green"}>
-//                                             <div>
-//                                                 <div>My name is {user.name}</div>
-//                                                 <div>My age is {user.age}</div>
-//                                             </div>
-//                                             <div>
-//                                                 <button onClick={()=>this.props.handleDeleteUser(user.id)}>Delete</button>
-//                                             </div>
-//                                             <hr/>
-//                                         </div>
-//                                     );
-                                
-//                             })
-//                         }
-//                     </>
-//                 }
-//             </div>
-//         );
-//     }
-// }
 
 const DisplayInfor = (props) => {
-    const {listUser} = props;
     const [isShowHideListUser, setShowHideListUser] = useState(true);
     const handleShowHideListUer = ()=>{
         setShowHideListUser(!isShowHideListUser);
     }
+
+    console.log("call me render");
+    useEffect(
+        ()=>{
+        if(props.listUser.length===0){
+            alert("done!");
+        }
+        console.log("me");
+    },[props.listUser]);
     return (
         <div className="display-infor-container">
             <div>
@@ -52,7 +27,7 @@ const DisplayInfor = (props) => {
             {isShowHideListUser && 
                 <>
                     {
-                        listUser.map((user)=>{
+                        props.listUser.map((user)=>{
                             return (
                                 <div key={user.id} className={parseInt(user.age) > 25 ? "red" : "green"}>
                                     <div>
