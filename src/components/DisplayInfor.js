3c2d65d40f5e72ebea25929c0284af5e657c2339
an_le 黎煌安 <an_le@accton.com.vn>
2025-11-21 09:08:56 +0700

    update 7

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,22 +1,41 @@
 import React from "react";
 
 class DisplayInfor extends React.Component{
+    state = {
+        isShowListUser: true
+    }
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser : !this.state.isShowListUser
+        })
+    }
     render(){
         const {listUser} = this.props;
-        console.log(listUser);
+        // console.log(listUser);
         return (
             <div>
-                {
-                    listUser.map((user)=>{
-                        console.log(user);
-                        return (
-                            <div key={user.id}>
-                                <div>My name is {user.name}</div>
-                                <div>My age is {user.age}</div>
-                                <hr/>
-                            </div>
-                        );
-                    })
+                <div>
+                    <span onClick={()=>{
+                        this.handleShowHide()
+                    }}>
+                    {this.state.isShowListUser === true ? "Hide list user" : "Show list user"}
+                    </span>
+                </div>
+                {this.state.isShowListUser && 
+                    <div>
+                        {
+                            listUser.map((user)=>{
+                                    return (
+                                        <div key={user.id} className={parseInt(user.age) > 25 ? "red" : "green"}>
+                                            <div>My name is {user.name}</div>
+                                            <div>My age is {user.age}</div>
+                                            <hr/>
+                                        </div>
+                                    );
+                                
+                            })
+                        }
+                    </div>
                 }
             </div>
         );
