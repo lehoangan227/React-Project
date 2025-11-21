abe2a990d3ea7cef2817bc34aac1a383409a4f7b
an_le 黎煌安 <an_le@accton.com.vn>
2025-11-21 13:06:07 +0700

    update 13

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,51 +2,66 @@ import React from "react";
 import './DisplayInfor.scss';
 import logo from './../logo.svg';
 
-class DisplayInfor extends React.Component{
-    state = {
-        isShowListUser: true
-    }
-    handleShowHide = () => {
-        this.setState({
-            isShowListUser : !this.state.isShowListUser
-        })
-    }
-    render(){
-        const {listUser} = this.props;
-        // console.log(listUser);
-        return (
-            <div className="display-infor-container">
-                {/* <img src={logo}/> */}
-                <div>
-                    <span onClick={()=>{
-                        this.handleShowHide()
-                    }}>
-                    {this.state.isShowListUser === true ? "Hide list user" : "Show list user"}
-                    </span>
-                </div>
-                {this.state.isShowListUser && 
-                    <>
-                        {
-                            listUser.map((user)=>{
-                                    return (
-                                        <div key={user.id} className={parseInt(user.age) > 25 ? "red" : "green"}>
-                                            <div>
-                                                <div>My name is {user.name}</div>
-                                                <div>My age is {user.age}</div>
-                                            </div>
-                                            <div>
-                                                <button onClick={()=>this.props.handleDeleteUser(user.id)}>Delete</button>
-                                            </div>
-                                            <hr/>
-                                        </div>
-                                    );
+// class DisplayInfor extends React.Component{
+    
+//     render(){
+//         console.log("Call me render")
+//         const {listUser} = this.props;
+//         // console.log(listUser);
+//         return (
+//             <div className="display-infor-container">
+//                 {true && 
+//                     <>
+//                         {
+//                             listUser.map((user)=>{
+//                                     return (
+//                                         <div key={user.id} className={parseInt(user.age) > 25 ? "red" : "green"}>
+//                                             <div>
+//                                                 <div>My name is {user.name}</div>
+//                                                 <div>My age is {user.age}</div>
+//                                             </div>
+//                                             <div>
+//                                                 <button onClick={()=>this.props.handleDeleteUser(user.id)}>Delete</button>
+//                                             </div>
+//                                             <hr/>
+//                                         </div>
+//                                     );
                                 
-                            })
-                        }
-                    </>
-                }
-            </div>
-        );
-    }
+//                             })
+//                         }
+//                     </>
+//                 }
+//             </div>
+//         );
+//     }
+// }
+
+const DisplayInfor = (props) => {
+    const {listUser} = props;
+    
+    return (
+        <div className="display-infor-container">
+            {true && 
+                <>
+                    {
+                        listUser.map((user)=>{
+                            return (
+                                <div key={user.id} className={parseInt(user.age) > 25 ? "red" : "green"}>
+                                    <div>
+                                        <div>My name is {user.name}</div>
+                                        <div>My age is {user.age}</div>
+                                    </div>
+                                    <div>
+                                        <button onClick={()=>props.handleDeleteUser(user.id)}>Delete</button>
+                                    </div>
+                                    <hr/>
+                                </div>
+                            );
+                        })
+                    }
+                </>
+            }
+        </div>
+    );
 }
 export default DisplayInfor;
