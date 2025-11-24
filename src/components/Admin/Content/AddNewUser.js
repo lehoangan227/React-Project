cff537240f7837d711f87ece76769c475a930cdb
an_le 黎煌安 <an_le@accton.com.vn>
2025-11-24 10:06:53 +0700

    update 19

--- src/components/Admin/Content/AddNewUser.js
+++ src/components/Admin/Content/AddNewUser.js
@@ -1,20 +1,74 @@
+import axios from "axios";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-const AddNewUser = () => {
-  const [show, setShow] = useState(false);
+const AddNewUser = (props) => {
+  const { show, setShow } = props;
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleClose = () => {
+    setShow(false);
+    setEmail("");
+    setPassword("");
+    setUsername("");
+    setRole("USER");
+    setImage("");
+    setPreviewImage("");
+  };
+
+  const handleAddNewUser = async () => {
+    // let data = {
+    //   email: email,
+    //   password: password,
+    //   username: username,
+    //   role: role,
+    //   userImage: image,
+    // };
+    // console.log("check data user:", data);
+
+    const data = new FormData();
+    data.append("email", email);
+    data.append("password", password);
+    data.append("username", username);
+    data.append("role", role);
+    data.append("userImage", image);
+
+    let res = await axios.post(
+      "http://localhost:8081/api/v1/participant",
+      data
+    );
+
+    console.log("check res add new user:", res);
+  };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [role, setRole] = useState("USER");
+  const [image, setImage] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+  const handleUploadImage = (event) => {
+    if (event.target && event.target.files && event.target.files[0]) {
+      setPreviewImage(URL.createObjectURL(event.target.files[0]));
+      setImage(event.target.files[0]);
+    } else {
+    }
+  };
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
+      {/* <Button variant="primary" onClick={handleShow}>
         Add New User
-      </Button>
+      </Button> */}
 
-      <Modal show={show} onHide={handleClose} size="xl" backdrop="static">
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="xl"
+        backdrop="static"
+        className="modal-add-user"
+      >
         <Modal.Header closeButton>
           <Modal.Title>Add New User</Modal.Title>
         </Modal.Header>
@@ -22,19 +76,38 @@ const AddNewUser = () => {
           <form className="row g-3">
             <div className="col-md-6">
               <label className="form-label">Email</label>
-              <input type="email" className="form-control" />
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
             </div>
             <div className="col-md-6">
               <label className="form-label">Password</label>
-              <input type="password" className="form-control" />
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
             </div>
             <div className="col-md-6">
               <label className="form-label">Username</label>
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
             </div>
             <div className="col-md-4">
               <label className="form-label">Role</label>
-              <select className="form-select">
+              <select
+                className="form-select"
+                value={role}
+                onChange={(event) => setRole(event.target.value)}
+              >
                 <option selected value="USER">
                   USER
                 </option>
@@ -42,8 +115,23 @@ const AddNewUser = () => {
               </select>
             </div>
             <div className="col-md-12">
-              <lable className="form-label">Upload File Image</lable>
-              <input type="file" hidden />
+              <label className="form-label label-upload" htmlFor="labelUpload">
+                Upload File Image
+              </label>
+              <input
+                type="file"
+                hidden
+                id="labelUpload"
+                onChange={(event) => handleUploadImage(event)}
+              />
+            </div>
+
+            <div className="col-md-12 img-preview">
+              {previewImage ? (
+                <img src={previewImage} />
+              ) : (
+                <span>Preview Image</span>
+              )}
             </div>
           </form>
         </Modal.Body>
@@ -51,7 +139,7 @@ const AddNewUser = () => {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handleAddNewUser}>
             Save Changes
           </Button>
         </Modal.Footer>
--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import AddNewUser from "./AddNewUser";
-
+import "./ManageUser.scss";
 const ManageUser = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="manage-user-container">
       <div className="title">Manage User</div>
       <div className="user-content">
-        <div>
-          <AddNewUser />
+        <div className="btn-add-new">
+          <button
+            className="btn btn-secondary"
+            onClick={() => setShowModal(true)}
+          >
+            Add New User
+          </button>
         </div>
-        <div>table users</div>
+        <div className="table-user-container">table users</div>
       </div>
+      <AddNewUser show={showModal} setShow={setShowModal} />
     </div>
   );
 };
